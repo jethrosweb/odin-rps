1fcfd97e1e78912e138f3eb905aaedffdfeb081f
Jethro Dean <jethroldean@gmail.com>
2022-07-10 13:34:47 +1000

    play round completed

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ const computerSelection = () => {
     return selection
 }
 
+let playerScore = 0
+let computerScore = 0
+
+// play round
+const playRound = () => {
+    let player = playerSelection()
+    let computer = computerSelection()
+
+    if (
+        player === 'rock' && computer === 'paper' ||
+        player === 'paper' && computer === 'scissors' ||
+        player === 'scissors' && computer === 'rock'    
+        ) {
+            console.log(`You lose! ${computer} beats ${player}`)
+            computerScore++
+    } else if (
+        player === 'scissors' && computer === 'paper' ||
+        player === 'paper' && computer === 'rock' ||
+        player === 'rock' && computer === 'scissors'
+        ) {
+            console.log(`You win! ${player} beats ${computer}`);
+            playerScore++
+        } else if (player === computer) {
+            console.log(`Tie!`);
+        }
+}
+
